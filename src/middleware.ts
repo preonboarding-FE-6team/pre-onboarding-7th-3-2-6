e377e96ed309ede934d7ba5dbd5996b9491e541a
2ujin <suddenlydisappear200@gmail.com>
2022-11-18 03:09:13 +0900

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { COOKIE_TOKEN_KEY } from '@repositories/CookieTokenRepository';
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith('/signin')) {
     if (request.cookies.get(COOKIE_TOKEN_KEY)) {
-      return NextResponse.redirect('/');
+      return NextResponse.redirect('https://effervescent-frangollo-1c8184.netlify.app/');
     }
   }
 
@@ -14,7 +14,7 @@ export function middleware(request: NextRequest) {
     request.nextUrl.pathname.startsWith('/users')
   ) {
     if (!request.cookies.get(COOKIE_TOKEN_KEY)) {
-      return NextResponse.redirect('/');
+      return NextResponse.redirect('https://effervescent-frangollo-1c8184.netlify.app/signin');
     }
   }
 }
